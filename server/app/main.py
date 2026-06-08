a0691e981445e89d50bfc321cd2a86ea1c1c157d
sejal <sakharwsejal@gmail.com>
2026-06-08 16:00:13 +0530

    register page and endpoint connected and solve some minor issue

--- server/app/main.py
+++ server/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.chat import router as chat_router
 from app.routes.auth import router as auth_router
 from app.routes.conversation import router as conv_router
@@ -9,6 +10,16 @@ load_dotenv()
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(chat_router, prefix="/chat", tags=["chat"])
 app.include_router(auth_router, prefix="/auth")
--- server/app/routes/auth.py
+++ server/app/routes/auth.py
@@ -15,60 +15,91 @@ router = APIRouter()
 @router.post(
     "/register",
 )
-async def register(request: RegisterRequest):
+async def register(request: RegisterRequest, response: Response):
 
-    existing_user = await get_user_by_email(request.email)
+    try:
 
-    if existing_user:
+        existing_user = await get_user_by_email(request.email)
 
-        raise HTTPException(status_code=400, detail="Email already exist")
+        if existing_user:
+            response.status_code = 409
+            return {
+                "message": "email already exist",
+                "success": False,
+            }
 
-    user = User(
-        user_id=str(uuid4()),
-        username=request.name,
-        email=request.email,
-        hashed_password=hash_password(request.password),
-        is_active=True,
-        created_at=datetime.utcnow(),
-        updated_at=datetime.utcnow(),
-    )
+        user = User(
+            user_id=str(uuid4()),
+            username=request.name,
+            email=request.email,
+            hashed_password=hash_password(request.password),
+            is_active=True,
+            created_at=datetime.utcnow(),
+            updated_at=datetime.utcnow(),
+        )
 
-    await create_user(user.model_dump())
+        await create_user(user.model_dump())
+        response.status_code = 200
+        return {
+            "success": True,
+            "message": "User registered successfully",
+        }
 
-    return {"success": True, "message": "User registered successfully"}
+    except Exception as e:
+        response.status_code = 400
+        return {
+            "success": False,
+            "message": "Error while registering",
+        }
 
 
 @router.post("/login")
 async def login(request: LoginRequest, response: Response):
-    user = await get_user_by_email(request.email)
 
-    if not user:
+    try:
 
-        raise HTTPException(status_code=404, detail="User not found")
+        user = await get_user_by_email(request.email)
 
-    if not verify_password(request.password, user["hashed_password"]):
+        if not user:
+            response.status_code = 404
+            return {
+                "message": "User not found",
+                "success": False,
+            }
 
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid credentials",
+        if not verify_password(request.password, user["hashed_password"]):
+            response.status_code = 401
+            return {
+                "message": "Password Invalid",
+                "success": False,
+            }
+
+        token = create_access_token(user["user_id"])
+
+        response.set_cookie(
+            key="access_token",
+            value=token,
+            httponly=True,
+            secure=False,
+            samesite="lax",
+            max_age=60 * 60 * 24 * 1,
         )
+        safe_user = dict(user)
+        safe_user.pop("hashed_password", None)
+        safe_user.pop("_id", None)
+        safe_user.pop("is_active", None)
+        safe_user.pop("created_at", None)
+        safe_user.pop("updated_at", None)
 
-    token = create_access_token(user["user_id"])
+        return {
+            "success": True,
+            "message": "User login successfully",
+            "user": safe_user,
+        }
+    except Exception as e:
 
-    response.set_cookie(
-        key="access_token",
-        value=token,
-        httponly=True,
-        secure=False,
-        samesite="lax",
-        max_age=60 * 60 * 24 * 1,
-    )
-    safe_user = dict(user)
-    safe_user.pop("hashed_password", None)
-    safe_user.pop("_id", None)
-
-    return {
-        "success": True,
-        "message": "User login successfully",
-        "user": safe_user,
-    }
+        response.status_code = 400
+        return {
+            "success": False,
+            "message": "Error while logging",
+        }
